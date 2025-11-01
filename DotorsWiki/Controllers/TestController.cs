2b3d13520cf430abe93bfb6f02c48bd2bbe74169
Atahar Islam Shihab <dihanhassan21@gmail.com>
2025-11-01 18:31:59 +0600

    add test controller

---
+++ DotorsWiki/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotorsWiki.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("hello")]
+        public async Task<IActionResult> GetHello()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
